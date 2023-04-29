24fb7062e6169438d0c05845a6e5567c5b80846d
unknown <brenomoraes97@yahoo.com>
2023-04-28 23:49:27 -0300

    feat: finishing register modal

--- src/components/RegisterModal/index.tsx
+++ src/components/RegisterModal/index.tsx
@@ -1,46 +1,69 @@
 import "./styles.css";
-
+import { HiOutlineX } from "react-icons/hi";
 const RegisterModal = ({ setShowRegisterModal }: any) => {
   return (
     <div className="overlay">
-      <div className="container-insert-register">
-        <section className="header">
-          <h1>"Adicionar Registro"</h1>
-          <button onClick={() => setShowRegisterModal(false)}>X</button>
+      <div className="register__container">
+        <section className="register__header">
+          <h1 className="register-modal__title">Adicionar Registro</h1>
+          <button
+            className="register__close"
+            onClick={() => setShowRegisterModal(false)}
+          >
+            <HiOutlineX size={30} />
+          </button>
         </section>
 
-        <section className="section-btn">
-          <button className="btn deposit-modal">Entrada</button>
+        <section className="register__types">
+          <button className="types__input">Entrada</button>
 
-          <button className="btn cash-out-modal">Saída</button>
+          <button className="types__output">Saída</button>
         </section>
 
-        <form className="section-form">
-          <label className="label" htmlFor="value">
-            Valor
-          </label>
-          <input name="valor" className="input" id="value" type="text" />
+        <form className="register__form">
+          <section className="form__section">
+            <label className="register__form-label" htmlFor="value">
+              Valor
+            </label>
+            <input
+              name="valor"
+              className="register-modal__input"
+              id="value"
+              type="text"
+            />
+          </section>
 
-          <label className="label">Categoria</label>
-          <select name="categoria_id" className="select">
-            <option>Selecione uma categoria</option>
-            <option>Categoria1</option>
-          </select>
+          <section className="form__section">
+            <label className="register__form-label">Categoria</label>
+            <select name="categoria_id" className="register-modal__input">
+              <option>Selecione uma categoria</option>
+              <option>Categoria1</option>
+            </select>
+          </section>
 
-          <label className="label" htmlFor="date">
-            Data
-          </label>
-          <input name="data" className="input" id="date" type="date" />
+          <section className="form__section">
+            <label className="register__form-label" htmlFor="date">
+              Data
+            </label>
+            <input
+              name="data"
+              className="register-modal__input"
+              id="date"
+              type="date"
+            />
+          </section>
 
-          <label className="label" htmlFor="description">
-            Descrição
-          </label>
-          <input
-            name="descricao"
-            className="input"
-            id="description"
-            type="text"
-          />
+          <section className="form__section">
+            <label className="register__form-label" htmlFor="description">
+              Descrição
+            </label>
+            <input
+              name="descricao"
+              className="register-modal__input"
+              id="description"
+              type="text"
+            />
+          </section>
 
           <button type="submit" className="btn-confirm">
             Confirmar
